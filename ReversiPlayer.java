7bcf460118d845e02ba779d8e09d27f9e38cb4cd
roeseth <41995115+roeseth@users.noreply.github.com>
2018-12-02 00:34:13 -0500

    a general reversi player class

--- ReversiPlayer.java
+++ ReversiPlayer.java
@@ -2,33 +2,14 @@
  * ReversiPlayer
  */
 public abstract class ReversiPlayer {
-    private int id;
     private String name;
-    private int winCount;
-    private int gameCount;
 
-    public int getId() {
-        return id;
+    public ReversiPlayer( String name ) {
+        this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void printStatistic() {
-        // TODO:
-    }
-
-    public void resetStatistic() {
-        // TODO:
-    }
-
-    public void recordWining() {
-        // TODO:
-    }
-
-    public void recordLosing() {
-        // TODO:
     }
 
     public abstract String getMove( ReversiBoard board, ReversiDiskType diskType );
