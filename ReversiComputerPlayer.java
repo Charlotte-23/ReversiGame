3f9c8678e6e0c35da43d64c9bb874555bfb8639c
roeseth <41995115+roeseth@users.noreply.github.com>
2018-12-02 00:33:50 -0500

    a computer player class template

---
+++ ReversiComputerPlayer.java
@@ -0,0 +1,16 @@
+/**
+ * ReversiComputerPlayer
+ */
+public class ReversiComputerPlayer extends ReversiPlayer {
+
+    public ReversiComputerPlayer( String name ) {
+        super( name );
+    }
+
+    public String getMove( ReversiBoard board, ReversiDiskType diskType ) {
+        String move = null;
+        // TODO:
+        return move;
+    }
+    
+}
