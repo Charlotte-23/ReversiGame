4d1ca191cb6eadf8142f8303c8a61d34d3d990e3
roeseth <41995115+roeseth@users.noreply.github.com>
2018-12-02 00:33:59 -0500

    a human player class template

---
+++ ReversiHumanPlayer.java
@@ -0,0 +1,16 @@
+/**
+ * ReversiHumanPlayer
+ */
+public class ReversiHumanPlayer extends ReversiPlayer {
+
+    public ReversiHumanPlayer( String name ) {
+        super( name );
+    }
+
+    public String getMove( ReversiBoard board, ReversiDiskType diskType ) {
+        String move = null;
+        // TODO:
+        return move;
+    }
+    
+}
