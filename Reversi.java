d04549abb3febe5a93cf627ce33372a12ba8e627
roeseth <41995115+roeseth@users.noreply.github.com>
2018-12-02 00:33:18 -0500

    simplified main skeleton update

--- Reversi.java
+++ Reversi.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class Reversi {
 
-    public static void printHelp() {
-        // TODO:
-    }
-
     public static void main( String[] args ) {
 
         Scanner keyboard = new Scanner( System.in );
         System.out.println( "Welcome to Reversi!" );
+
+        game:
         while ( true ) {
-            ReversiBoard testBoard = new ReversiBoard(12, new ReversiHumanPlayer(), new ReversiComputerPlayer() );
-            testBoard.drawBoard();
+            ReversiBoard board = new ReversiBoard( 8, new ReversiHumanPlayer( "testname1" ), new ReversiComputerPlayer( "testname2" ) );
+            board.drawBoard();
+
+            // TODO: game logic here
+
+            System.out.print( "Play again? (y/n): " );
+            String flag;
+            while ( !(flag = keyboard.next()).equals( "y" ) && !flag.equals( "n" ) ) {
+                keyboard.nextLine();
+                System.out.print( "No a valaid answer, input again: " );
+            }
+            if ( flag.equals( "n" ) ) {
+                break game;
+            }
         }
     }
 }
