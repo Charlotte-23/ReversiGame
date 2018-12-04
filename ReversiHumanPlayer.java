42ea3a430dfb39f343e94710841af234c1be5924
xsygloria <44788125+xsygloria@users.noreply.github.com>
2018-12-04 17:25:01 -0500

    Update ReversiHumanPlayer.java

--- ReversiHumanPlayer.java
+++ ReversiHumanPlayer.java
@@ -8,9 +8,17 @@ public class ReversiHumanPlayer extends ReversiPlayer {
     }
 
     public String getMove( ReversiBoard board, ReversiDiskType diskType ) {
-        String move = null;
+        String validInputA = "abcdefghi";
+        String validInputB = "123456789";
         // TODO:
-        return move;
-    }
-    
+        Scanner s = new Scanner(System.in);
+        for (;;) {
+            System.out.Println("where do you wanna place your disk? (a1-i9)");
+            String move = s.nextLine();
+            if (validInputA.indexOf(ans.charAt(0)) != -1 &&
+                validInputB.indexOf(ans.charAt(1)) != -1) {
+                return move.subString(0,2);
+            }
+        }
+    } 
 }
