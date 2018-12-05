ca326d614193d2288bb7881c3743e9afde72513b
xsygloria <shangyuan@brandeis.edu>
2018-12-04 20:46:56 -0500

    fix againgain

--- ReversiHumanPlayer.java
+++ ReversiHumanPlayer.java
@@ -11,17 +11,8 @@ public class ReversiHumanPlayer extends ReversiPlayer {
     
     public String getMove( ReversiBoard board, ReversiDiskType diskType ) {
         Scanner s = new Scanner(System.in);
-        while ( true ) {
-            System.out.print("where do you wanna place your disk? (e.g. a1, 1a):");
-            String move = s.nextLine().substring(0,2);
-            try {
-                if ( board.isValidPosition(move) ) {
-                    return move;
-                }
-            } catch (Exception e) {
-            } finally {
-                System.out.print( "Not a valid move, input again: " );
-            }
-        }
+        System.out.print("where do you wanna place your disk? (e.g. a1, 1a):");
+        String move = s.nextLine().substring(0,2);
+        return move;
     } 
 }
