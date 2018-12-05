56147aa7d692902dab9bf2c6b708aa8b42aea321
Charlotte-Mao <maozhuolin@outlook.com>
2018-12-04 21:38:21 -0500

    Update Reversi.java
    set the size to the next even number.

--- Reversi.java
+++ Reversi.java
@@ -9,9 +9,13 @@ public class Reversi {
 
         game:
         while ( true ) {
-            System.out.print( "Input the size of the game board (2-26) : " );
+            System.out.print( "Input the size of the game board (even integer between 2-26) : " );
             int size = keyboard.nextInt();
             keyboard.nextLine();
+            if(size%2 == 1){
+              size++;
+              System.out.println("You typed in an odd integer, round up to the next even number: " + size);
+            }
             System.out.print( "Enter the name of player X:" );
             String name1 = keyboard.nextLine();
             System.out.print( "Enter the name of player O:" );
@@ -19,7 +23,7 @@ public class Reversi {
             ReversiBoard board = new ReversiBoard( size, new ReversiHumanPlayer( "name1" ), new ReversiHumanPlayer( "name2" ) );
             while( !board.isOver() ) {
                 board.drawBoard();
-                while ( !board.placeDisk( board.getCurrentPlayer().getMove(board, board.getCurrentType()), 
+                while ( !board.placeDisk( board.getCurrentPlayer().getMove(board, board.getCurrentType()),
                                           board.getCurrentType() ) ) {
                     System.out.println( "Invalid move, try again" );
                 }
